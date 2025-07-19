255f4c520c499de4e45e02b907a1c18dbed9a03d
AryaPiyus <aryapiyush2930@gmail.com>
2025-07-20 02:46:32 +0530

    prisma client generated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,25 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+app.post('/api/v1/signup', (c):any=>{
+
+} )
+
+app.post('/api/v1/signin',(c):any=>{
+
+})
+
+app.post('/api/v1/blog',(c):any=>{
+
+})
+
+app.put('/api/v1/blog' , (c):any=>{
+
+})
+
+app.get('/api/v1/blog/:id' , (c):any=>{
+
 })
 
 export default app
