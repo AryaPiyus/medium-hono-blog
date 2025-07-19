4c5957e6c0200c7e99f1d0d8e0431633e6b399f9
AryaPiyus <aryapiyush2930@gmail.com>
2025-07-20 04:47:35 +0530

    user creation and authentication endpoints done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,82 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { jwt , sign , verify , decode} from 'hono/jwt'
 
-const app = new Hono()
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string
+  },
+  Variables: {
+    userId: string | number
+  }
+}>()
+
+app.use('/api/v1/blog/*' , async (c, next)=>{
+  const header = c.req.header("Authorization") || ""
+  if(!header){
+    c.status(401)
+    return c.json({error: "authorization token missing"})
+  }
+  const token = header.split(' ')[1]
+  if(!token){
+    c.status(401)
+    return c.json({error:"unauthorized"})
+  }
+
+  const response = await verify( token , c.env.JWT_SECRET)
+  if(!response.id){
+    c.status(403)
+    return c.json({error:"user does not exist"})
+  }
+  c.set('userId' , String(response.id))
+  await next()
+})
 
 
-app.post('/api/v1/signup', (c):any=>{
+app.post('/api/v1/signup', async (c)=>{
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
 
-} )
+  const body = await c.req.json()
 
-app.post('/api/v1/signin',(c):any=>{
+  const user = await prisma.user.create({
+    data:{
+      email: body.email,
+      password: body.password
+    }
+  })
+  const token = await sign({id:user.id} , c.env.JWT_SECRET)
+  return c.json({jwt:token})
+})
+
+app.post('/api/v1/signin',async (c)=>{
+
+  const prisma = new PrismaClient({
+    datasourceUrl : c.env.DATABASE_URL ,
+  }).$extends(withAccelerate())
+  const body = await c.req.json()
+
+  const user = await prisma.user.findUnique({
+    where:{
+      email : body.email ,
+      password : body.password,
+    }
+  })
+
+  if(!user){
+    c.status(403)
+    return c.json({error:"user does not exist"})
+  }
+  const token = await sign({id:user.id} , c.env.JWT_SECRET)
+  return c.json({jwt:token})
 
 })
 
-app.post('/api/v1/blog',(c):any=>{
+app.post('/api/v1/blog',async (c)=>{
 
 })
 
